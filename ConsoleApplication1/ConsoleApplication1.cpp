4850168470b69c718a8b209f6cba54f2053bbfa7
Spep <ilyaesegj@gmail.com>
2021-11-30 21:06:19 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+
+{
+    int arrayLen;
+    cin >> arrayLen;
+    int* array = new int[arrayLen];
+    for (int i = 0;i < arrayLen;++i)
+    {
+        cout << (array[i] = rand() % arrayLen + 1);
+    }
+    cout << endl;
+    int pairCounter = 0;
+    for (int i = 0;i < arrayLen - 1;i++)
+    {
+        if (array[i] % 2 == 0 && array[i + 1] % 2 == 0)
+        {
+            pairCounter += 1;
+        }
+    }
+    cout << pairCounter << endl;
+    delete[] array;
+    system("pause");
+    return 0;
+}
